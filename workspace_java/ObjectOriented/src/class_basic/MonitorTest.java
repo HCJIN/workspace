372158e2ad808f9d2d57704722c2e4a7c1fe96ed
HCJIN <ghkdckswls02@naver.com>
2024-06-07 16:19:23 +0900

    1

---
+++ workspace_java/ObjectOriented/src/class_basic/MonitorTest.java
@@ -0,0 +1,13 @@
+package class_basic;
+
+public class MonitorTest {
+    public static void main(String[] args) {
+
+        //모니터 설계도에서 모니터 객체 생성
+        //객체 생성 문법 : 클래스명 객체명 = new 크래스명();
+
+        Monitor m1 = new Monitor();
+
+
+    }
+}
